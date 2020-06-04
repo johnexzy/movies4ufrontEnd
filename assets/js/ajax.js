e0c9e1509f695d6f64c95300d4a9e88999b005f7
Johnexzy <obajohn75@gmail.com>
2020-06-04 21:23:58 +0100

    developing

--- assets/js/ajax.js
+++ assets/js/ajax.js
@@ -1,11 +1,13 @@
 $(function(){
     $.ajax({
-        url: 'http://127.0.0.1:8090/api/music',
+        url: 'http://127.0.0.1:8090/api/music/limit/4',
         type: 'GET'
       })
       .done(function(data){
-          $.each(data, function(){
-              $(".music-dom").append(function(){
+          $.each(data, function( key, music){
+              
+
+                      $("#music-dom").append(
                   `
                     <div class="row">
                         <div class="col-sm-12">
@@ -13,27 +15,27 @@ $(function(){
                             <div class="row">
                                 <div class="col-sm-5 pr-2">
                                 <div class="rotate-img">
-                                    <img src="assets/images/dashboard/home_19.jpg" alt="thumb"
+                                    <img src="http://127.0.0.1:8090/${music.images[0]}" alt="thumb"
                                     class="img-fluid w-100" />
                                 </div>
                                 </div>
                                 <div class="col-sm-7 pl-2">
                                 <p class="fs-16 font-weight-600 mb-0">
-                                    Online shopping ..
+                                    ${music.music_name}
                                 </p>
                                 <p class="fs-13 text-muted mb-0">
-                                    <span class="mr-2">Photo </span>10 Minutes ago
+                                    <span class="mr-2">${music.artist} </span>10 Minutes ago
                                 </p>
-                                <p class="mb-0 fs-13">
-                                    Lorem Ipsum has been
-                                </p>
+                                
                                 </div>
                             </div>
                             </div>
                         </div>
                     </div>
                   `
-              })
+              
+                  )
+                  
           })
       })
       .fail(function(params) {

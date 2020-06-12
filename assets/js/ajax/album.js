04b2c68dacd501f10608e0e1671a4d31c02941b8
Johnexzy <obajohn75@gmail.com>
2020-06-12 19:37:05 +0100

    Building PAGINATION

--- assets/js/ajax/album.js
+++ assets/js/ajax/album.js
@@ -101,58 +101,59 @@ $(function(){
         if (link == "") {
             return false
         }
-        $.ajax({
-            url: `http://127.0.0.1:8090/api/v1/album/${link}`,
-            type: 'GET',
-            beforeSend : ()=>{
-                $(".show-album").html(`
+        $(".show-album").html(`
                     <div class="album-loader">
                         <div class="d-flex justify-content-center">
                             <img src="./assets/images/loader copy.gif" alt="">
                         </div>
                     </div>
                 `)
-            }
+        $.ajax({
+            url: `http://127.0.0.1:8090/api/v1/album/${link}`,
+            type: 'GET',
         })
         .done((data)=>{
+            $("body, html").animate({
+                scrollTop: 0
+            })
             $(".show-album").html("")
-        $.each(data.data, (key, album)=>{
-            $(".show-album").append(`
-            <div class="row">
-            <div class="col-sm-4 grid-margin">
-              <div class="rotate-img">
-              
-                <img
-                  src="http://127.0.0.1:8090/${album.images[0]}"
-                  alt="banner"
-                  class="img-fluid"
-                />
-              </div>
+            $.each(data.data, (key, album)=>{
+                $(".show-album").append(`
+                <div class="row">
+                <div class="col-sm-4 grid-margin">
+                <div class="rotate-img">
+                
+                    <img
+                    src="http://127.0.0.1:8090/${album.images[0]}"
+                    alt="banner"
+                    class="img-fluid"
+                    />
+                </div>
+                </div>
+                <div class="col-sm-8 grid-margin">
+                <h2 class="font-weight-600 mb-2">
+                    ${album.album_name}
+                </h2>
+                <p class="fs-13 text-muted mb-0">
+                    <span class="mr-2">Release Year: </span>${album.year}
+                </p>
+                <p class="fs-15">
+                    ${album.album_details}
+                </p>
+                </div>
             </div>
-            <div class="col-sm-8 grid-margin">
-              <h2 class="font-weight-600 mb-2">
-                ${album.album_name}
-              </h2>
-              <p class="fs-13 text-muted mb-0">
-                <span class="mr-2">Release Year: </span>${album.year}
-              </p>
-              <p class="fs-15">
-                ${album.album_details}
-              </p>
-            </div>
-          </div>
-            `)
-        })
-        data.links.prev == null ?
-        $(".prev").find("#pagelink").val("")
-        :
-        $(".prev").find("#pagelink").val(`${data.links.prev}`)
+                `)
+            })
+            data.links.prev == null ?
+            $(".prev").find("#pagelink").val("")
+            :
+            $(".prev").find("#pagelink").val(`${data.links.prev}`)
 
-        data.links.next == null ?
-        $(".next").find("#pagelink").val("")
-        :
-        $(".next").find("#pagelink").val(`${data.links.next}`)
-        })
+            data.links.next == null ?
+            $(".next").find("#pagelink").val("")
+            :
+            $(".next").find("#pagelink").val(`${data.links.next}`)
+            })
         .fail((err)=>{
             console.log(err)
             alert("Something Went Wrong! Try Again")

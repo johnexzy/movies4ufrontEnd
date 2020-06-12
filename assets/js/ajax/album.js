079093a37620e0909671e3288e55c4061fc69603
Johnexzy <obajohn75@gmail.com>
2020-06-12 19:25:55 +0100

    Building PAGINATION

--- assets/js/ajax/album.js
+++ assets/js/ajax/album.js
@@ -1,6 +1,6 @@
 $(function(){
     $.ajax({
-        url: 'http://127.0.0.1:8090/api/v1/album/pages/2',
+        url: 'http://127.0.0.1:8090/api/v1/album/pages/1',
         type: 'GET',
         beforeSend: ()=>{
 
@@ -36,47 +36,21 @@ $(function(){
           </div>
             `)
         })
-        $(".pagination").html("")
+        // $(".pagination").html("")
         data.links.prev == null ?
-        $(".pagination").append(`
-            <li class="page-item">
-                <a class="page-link disabled" href="#">
-                    <i class="mdi mdi-arrow-left-bold"></i>
-                </a>
-            </li>
-        `)
+        $(".prev").find("#pagelink").val("")
         :
-        $(".pagination").append(`
-            <li class="page-item">
-            <input id="pagelink" value="${data.links.prev}" type="hidden">
-
-                <a class="page-link" href="#">
-                    <i class="mdi mdi-arrow-left-bold"></i>
-                </a>
-            </li>
-        `)
+        $(".prev").find("#pagelink").val(`${data.links.prev}`)
 
         data.links.next == null ?
-        $(".pagination").append(`
-            <li class="page-item">
-                <a class="page-link disabled">
-                    <i class="mdi mdi-arrow-right-bold"></i>
-                </a>
-            </li>
-        `)
+        $(".next").find("#pagelink").val("")
         :
-        $(".pagination").append(`
-            <li class="page-item">
-            <input id="pagelink" value="${data.links.next}" type="hidden">
-                <a class="page-link" >
-                    <i class="mdi mdi-arrow-right-bold"></i>
-                </a>
-            </li>
-        `)
+        $(".next").find("#pagelink").val(`${data.links.next}`)
     })
     .fail((err)=>{
 
     })
+    
     $.ajax({
         url: 'http://127.0.0.1:8090/api/v1/album/popular/6',
         type: 'GET',
@@ -109,16 +83,80 @@ $(function(){
     })
     .fail((err)=>{
 
-    })
+    });
     
-
     //Control Pagination Using AJAX || FETCH API
 
+})
+
+$(function(){
+    
     $(".page-item").on("click", function(){
-        if (!$(this).find("#pagelink")) {
+        // if (!$(this).find("#pagelink")) {
+        //     return false
+        // }
+        // alert("john")
+        let link = $(this).find("#pagelink").val();
+        // alert(link)
+        if (link == "") {
             return false
         }
-        let link = $(this).find("#pagelink").val()
+        $.ajax({
+            url: `http://127.0.0.1:8090/api/v1/album/${link}`,
+            type: 'GET',
+            beforeSend : ()=>{
+                $(".show-album").html(`
+                    <div class="album-loader">
+                        <div class="d-flex justify-content-center">
+                            <img src="./assets/images/loader copy.gif" alt="">
+                        </div>
+                    </div>
+                `)
+            }
+        })
+        .done((data)=>{
+            $(".show-album").html("")
+        $.each(data.data, (key, album)=>{
+            $(".show-album").append(`
+            <div class="row">
+            <div class="col-sm-4 grid-margin">
+              <div class="rotate-img">
+              
+                <img
+                  src="http://127.0.0.1:8090/${album.images[0]}"
+                  alt="banner"
+                  class="img-fluid"
+                />
+              </div>
+            </div>
+            <div class="col-sm-8 grid-margin">
+              <h2 class="font-weight-600 mb-2">
+                ${album.album_name}
+              </h2>
+              <p class="fs-13 text-muted mb-0">
+                <span class="mr-2">Release Year: </span>${album.year}
+              </p>
+              <p class="fs-15">
+                ${album.album_details}
+              </p>
+            </div>
+          </div>
+            `)
+        })
+        data.links.prev == null ?
+        $(".prev").find("#pagelink").val("")
+        :
+        $(".prev").find("#pagelink").val(`${data.links.prev}`)
+
+        data.links.next == null ?
+        $(".next").find("#pagelink").val("")
+        :
+        $(".next").find("#pagelink").val(`${data.links.next}`)
+        })
+        .fail((err)=>{
+            console.log(err)
+            alert("Something Went Wrong! Try Again")
+        })
 
     })
 })

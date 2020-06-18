fd9689acc8e9398cb58db67e43551eb6d493bbc6
Johnexzy <obajohn75@gmail.com>
2020-06-18 14:50:53 +0100

    Building getMovie Controller

--- Controller/Movie/getMovie.php
+++ Controller/Movie/getMovie.php
@@ -3,5 +3,13 @@ namespace Controller\Movie;
 
 
 class getMovie{
-    
+    private $short_url;
+    public function __construct(String $short_url = null) {
+        $this->short_url = $short_url;
+    }
+    public function makeRequest()
+    {
+        $res = file_get_contents("http://127.0.0.1:8090/api/v1/videos/url/$this->short_url");
+        return $res;
+    }
 }

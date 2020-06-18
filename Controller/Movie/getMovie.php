247c64f66ca51cfed79625b19184507fc3c37810
Johnexzy <obajohn75@gmail.com>
2020-06-18 21:37:45 +0100

    Building View Controllers

--- Controller/Movie/getMovie.php
+++ Controller/Movie/getMovie.php
@@ -67,122 +67,7 @@ class getMovie{
           <body>
             <div class="container-scroller">
               <!-- partial:../partials/_navbar.html -->
-              <header id="header">
-                  <div class="container">
-                    <nav class="navbar navbar-expand-lg navbar-light">
-                      <div class="navbar-top">
-                        <div class="d-flex justify-content-between align-items-center">
-                          <ul class="navbar-top-left-menu">
-                            <li class="nav-item">
-                              <a href="./index-inner.html" class="nav-link">Advertise</a>
-                            </li>
-                            <li class="nav-item">
-                              <a href="./aboutus.html" class="nav-link">About</a>
-                            </li>
-                            <li class="nav-item">
-                              <a href="#" class="nav-link">Events</a>
-                            </li>
-                            <li class="nav-item">
-                              <a href="#" class="nav-link">Write for Us</a>
-                            </li>
-                            <li class="nav-item">
-                              <a href="#" class="nav-link">In the Press</a>
-                            </li>
-                          </ul>
-                          <ul class="navbar-top-right-menu">
-                            <li class="nav-item">
-                              <a href="#" class="nav-link"><i class="mdi mdi-magnify"></i></a>
-                            </li>
-                            <li class="nav-item">
-                              <a href="#" class="nav-link">Login</a>
-                            </li>
-                            <li class="nav-item">
-                              <a href="#" class="nav-link">Sign in</a>
-                            </li>
-                          </ul>
-                        </div>
-                      </div>
-                      <div class="navbar-bottom">
-                        <div class="d-flex justify-content-between align-items-center">
-                          <div>
-                            <a class="navbar-brand" href="#"
-                              ><img src="../../assets/images/untitled.png" alt=""
-                            /></a>
-                          </div>
-                          <div>
-                            <button
-                              class="navbar-toggler"
-                              type="button"
-                              data-target="#navbarSupportedContent"
-                              aria-controls="navbarSupportedContent"
-                              aria-expanded="false"
-                              aria-label="Toggle navigation"
-                            >
-                              <span class="navbar-toggler-icon"></span>
-                            </button>
-                            <div
-                              class="navbar-collapse justify-content-center collapse"
-                              id="navbarSupportedContent"
-                            >
-                              <ul
-                                class="navbar-nav d-lg-flex justify-content-between align-items-center"
-                              >
-                                <li>
-                                  <button class="navbar-close">
-                                    <i class="mdi mdi-close"></i>
-                                  </button>
-                                </li>
-                                <li class="nav-item active">
-                                  <a class="nav-link" href="../index.html">Home</a>
-                                </li>
-                                <li class="nav-item">
-                                  <a class="nav-link" href="./magazine.html">MAGAZINE</a>
-                                </li>
-                                <li class="nav-item">
-                                  <a class="nav-link" href="./business.html">Business</a>
-                                </li>
-                                <li class="nav-item">
-                                  <a class="nav-link" href="./sports.html">Sports</a>
-                                </li>
-                                <li class="nav-item">
-                                  <a class="nav-link" href="./art.html">Art</a>
-                                </li>
-                                <li class="nav-item">
-                                  <a class="nav-link" href="./politics.html">POLITICS</a>
-                                </li>
-                                <li class="nav-item">
-                                  <a class="nav-link" href="./travel.html">Travel</a>
-                                </li>
-                                <li class="nav-item">
-                                  <a class="nav-link" href="./contactus.html">Contact</a>
-                                </li>
-                              </ul>
-                            </div>
-                          </div>
-                          <ul class="social-media">
-                            <li>
-                              <a href="#">
-                                <i class="mdi mdi-facebook"></i>
-                              </a>
-                            </li>
-                            <li>
-                              <a href="#">
-                                <i class="mdi mdi-youtube"></i>
-                              </a>
-                            </li>
-                            <li>
-                              <a href="#">
-                                <i class="mdi mdi-twitter"></i>
-                              </a>
-                            </li>
-                          </ul>
-                        </div>
-                      </div>
-                    </nav>
-                  </div>
-                </header>
-        
-              <!-- partial -->
+              
               <div class="flash-news-banner">
                 <div class="container">
                   <div class="d-lg-flex align-items-center justify-content-between">
@@ -206,7 +91,9 @@ class getMovie{
                     <div class="col-sm-12">
                       <div class="card" data-aos="fade-up">
                       <div class="card-header">
-                        $name
+                        <p class="font-weight-bold" style="text-align:center">
+                            $name
+                        </p>
                       </div>
                         <div class="card-body">
                         <div class="row">

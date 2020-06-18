40e03d312cea68ddf9dd4326081d4fb83a663521
Johnexzy <obajohn75@gmail.com>
2020-06-18 15:04:38 +0100

    Building View Controllers

--- Controller/Movie/getMovie.php
+++ Controller/Movie/getMovie.php
@@ -7,13 +7,17 @@ class getMovie{
     public function __construct(String $short_url = null) {
         $this->short_url = $short_url;
     }
-    public function makeRequest()
+    public static function makeRequest($short_url)
     {
-        $res = file_get_contents("http://127.0.0.1:8090/api/v1/videos/url/$this->short_url");
+        $res = file_get_contents("http://127.0.0.1:8090/api/v1/videos/url/$short_url");
         return $res;
     }
     public function bodyParser()
     {
+        return ?>
         
+        <?php
+
+
     }
 }
---
+++ view/index.php
@@ -0,0 +1,40 @@
+<?php
+
+// use Src\Controller\ViewController\ViewController;
+
+$uri           = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$uri           = explode('/', $uri);
+$requestMethod = $_SERVER['REQUEST_METHOD'];
+
+if ($uri[2] == 'movies') {
+    if (isset($uri[3])) {
+        $short_url = (String)$uri[3];
+    } else {
+        header("HTTP/1.1 404 Not Found");
+        exit();
+    }
+
+    // pass the request method and user ID to the CarouselController and process the HTTP request:
+    // $carousel = new ViewController($dbConnection, $short_url, $id, "../../", $uri[2]);
+    // echo($carousel->showView());
+} elseif ($uri[2] == 'news') {
+    $id        = null;
+    $short_url = null;
+    if (isset($uri[3])) {
+        if ($requestMethod == "GET") {
+            // $short_url = (intval($uri[3]) == 0) ? (String) $uri[3] : null;
+            // $id = ($short_url == null) ? (int) $uri[3] : null;
+            $short_url = (String)$uri[3];
+        }
+    } else {
+        header("HTTP/1.1 404 Not Found");
+        exit();
+    }
+
+    // pass the request method and user ID to the CarouselController and process the HTTP request:
+    // $news = new ViewController($dbConnection, $short_url, $id, "../../", $uri[2]);
+    // echo($news->showView());
+} else {
+    header("HTTP/1.1 404 Not Found");
+    exit();
+}

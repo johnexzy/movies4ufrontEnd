44f5eeec5f652024ac863beff0eb94108ffc9ef0
Johnexzy <obajohn75@gmail.com>
2020-06-20 21:42:11 +0100

    comments building

--- view/index.php
+++ view/index.php
@@ -1,6 +1,7 @@
 <?php
 require '../vendor/autoload.php';
 
+use Src\Album\getAlbum;
 use Src\Movie\getMovie;
 $uri           = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $uri           = explode('/', $uri);
@@ -9,25 +10,24 @@ $requestMethod = $_SERVER['REQUEST_METHOD'];
 if ($uri[2] == 'movies') {
     if (isset($uri[3])) {
         $short_url = (String)$uri[3];
+        $movieView = new getMovie($short_url);
+        echo($movieView->bodyParser());
     } else {
         header("HTTP/1.1 404 Not Found");
         exit();
     }
-    $movieView = new getMovie($short_url);
-    echo($movieView->bodyParser());
-} elseif ($uri[2] == 'news') {
-    $id        = null;
-    $short_url = null;
+    
+}
+elseif ($uri[2] == 'albums') {
     if (isset($uri[3])) {
-        if ($requestMethod == "GET") {
-            // $short_url = (intval($uri[3]) == 0) ? (String) $uri[3] : null;
-            // $id = ($short_url == null) ? (int) $uri[3] : null;
-            $short_url = (String)$uri[3];
-        }
+        $short_url = (String)$uri[3];
+        $movieView = new getAlbum($short_url);
+        echo($movieView->bodyParser());
     } else {
         header("HTTP/1.1 404 Not Found");
         exit();
     }
+    
 
     // pass the request method and user ID to the CarouselController and process the HTTP request:
     // $news = new ViewController($dbConnection, $short_url, $id, "../../", $uri[2]);

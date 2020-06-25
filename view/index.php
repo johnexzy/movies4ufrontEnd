7b5b9c865b5506d07465ca9b9aa9d0fc1a93b0fe
Johnexzy <obajohn75@gmail.com>
2020-06-25 21:14:39 +0100

    Modified

--- view/index.php
+++ view/index.php
@@ -8,36 +8,37 @@ use Src\Music\getMusic;
 $uri           = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $uri           = explode('/', $uri);
 $requestMethod = $_SERVER['REQUEST_METHOD'];
-
+$notFound = \file_get_contents(__DIR__."\..\pages/404new.html", true);
 if ($uri[2] == 'movies') {
-    if (isset($uri[3])) {
+    if (isset($uri[3]) && strlen($uri[3]) > 3) {
         $short_url = (String)$uri[3];
         $movieView = new getMovie($short_url);
         echo($movieView->bodyParser());
     } else {
-        header("HTTP/1.1 404 Not Found");
-        exit();
-    }
+        echo <<<HTML
+          $notFound
+      HTML;
+  }
     
 }
 elseif ($uri[2] == 'albums') {
-    if (isset($uri[3])) {
+    if (isset($uri[3]) && strlen($uri[3]) > 3) {
         $short_url = (String)$uri[3];
         $movieView = new getAlbum($short_url);
         echo($movieView->bodyParser());
     } else {
-        header("HTTP/1.1 404 Not Found");
-        exit();
-    }
+        echo <<<HTML
+          $notFound
+      HTML;
+  }
 
 }
 elseif ($uri[2] == 'music') {
-    if (isset($uri[3])) {
+    if (isset($uri[3])  && strlen($uri[3]) > 3) {
         $short_url = (String)$uri[3];
         $movieView = new getMusic($short_url);
         echo($movieView->bodyParser());
     } else {
-        $notFound = \file_get_contents(__DIR__."\..\pages/404new.html", true);
           echo <<<HTML
             $notFound
         HTML;
@@ -45,7 +46,6 @@ elseif ($uri[2] == 'music') {
 
 }
  else {
-    $notFound = \file_get_contents(__DIR__."\..\pages/404new.html", true);
           echo <<<HTML
             $notFound
         HTML;

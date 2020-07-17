a7eadc87f647333b086c5f19bb102084310dba15
Johnexzy <obajohn75@gmail.com>
2020-07-17 22:16:46 +0100

    edited

--- assets/js/ajax.js
+++ assets/js/ajax.js
@@ -16,7 +16,7 @@ $(function () {
     .done((data) => {
       $.each(data, function(key, series){
         $(".series").append(`
-          <div class="row border-bottom mb-3">
+          <div class="row  ${key !==3 ? "border-bottom mb-3": ""} ">
             <div class="col-sm-4 grid-margin">
               <div class="position-relative">
                 <div class="rotate-img">
@@ -28,6 +28,10 @@ $(function () {
               <h2 class="mb-2 font-weight-600">
                 ${series.series_name}
               </h2>
+              
+              <div class="fs-13 mb-2">
+                <span class="mr-2">Total Season: </span><b>${series.series.length}</b>
+              </div>
               <p class="mb-0">
                 ${series.series_details}...
               </p>

479be5d132365a36d3eb4739748fc6156bed4733
Johnexzy <obajohn75@gmail.com>
2020-07-18 12:04:49 +0100

    edited getMusic.php

--- view/index.php
+++ view/index.php
@@ -9,6 +9,14 @@ $uri           = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $uri           = explode('/', $uri);
 $requestMethod = $_SERVER['REQUEST_METHOD'];
 $notFound = \file_get_contents(__DIR__."\..\pages/404new.html", true);
+
+/**
+ ** View Router**
+ * Music
+ * Album
+ * Movie
+ * Series
+ */
 if ($uri[2] == 'movies') {
     if (isset($uri[3]) && strlen($uri[3]) > 3) {
         $short_url = (String)$uri[3];
@@ -45,7 +53,19 @@ elseif ($uri[2] == 'music') {
     }
 
 }
- else {
+elseif ($uri[2] == 'music') {
+    if (isset($uri[3])  && strlen($uri[3]) > 3) {
+        $short_url = (String)$uri[3];
+        $movieView = new getMusic($short_url);
+        echo($movieView->bodyParser());
+    } else {
+          echo <<<HTML
+            $notFound
+        HTML;
+    }
+
+}
+else {
           echo <<<HTML
             $notFound
         HTML;

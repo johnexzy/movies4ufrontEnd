2b98405c40db662e16539f47991fd7632f77fdb4
Johnexzy <obajohn75@gmail.com>
2020-08-11 21:54:36 +0100

    modified

--- src/Search/Search.php
+++ src/Search/Search.php
@@ -49,7 +49,34 @@ class Search{
           //Music
           if ($group["group"] == "music") {
             foreach ($group["data"] as $key => $music) {
-              
+              $image = $music["images"][0];
+              $musicHtml .=<<<HTML
+                <div class="row">
+                  <div class="col-sm-4 grid-margin">
+                      <a href="/view/music/$music[short_url]" style="text-decoration:none; color: inherit">
+                          <div class="rotate-img">
+                              <img
+                              src="http://127.0.0.1:8090/$image"
+                              alt="banner"
+                              class="img-fluid"
+                              />
+                          </div>
+                      </a>
+                  </div>
+                  <div class="col-sm-8 grid-margin">
+                      <h2 class="font-weight-600 mb-2">
+                          <a href="/view/music/$music[short_url]" style="text-decoration:none; color: inherit">
+                              $music[music_name]
+                          </a>
+                      </h2>
+                    
+                      <p class="L5 mb-0">
+                          <i class="mdi mdi-artist"></i> <span class="fs-16 mr-2 text-muted">$music[artist]</span>
+                      </p>
+                  </div>
+                </div>
+                <hr>
+              HTML;
             }
           }
           //Series
@@ -59,9 +86,36 @@ class Search{
             }
           }
           //Movies
-          if ($group["group"] == "music") {
-            foreach ($group["data"] as $key => $music) {
-              
+          if ($group["group"] == "movies") {
+            foreach ($group["data"] as $key => $movie) {
+              $image = $movie["images"][0];
+              $moviesHtml .=<<<HTML
+                <div class="row">
+                  <div class="col-sm-4 grid-margin">
+                      <a href="/view/movies/$movie[short_url]" style="text-decoration:none; color: inherit">
+                          <div class="rotate-img">
+                              <img
+                              src="http://127.0.0.1:8090/$image"
+                              alt="banner"
+                              class="img-fluid"
+                              />
+                          </div>
+                      </a>
+                  </div>
+                  <div class="col-sm-8 grid-margin">
+                      <h2 class="font-weight-600 mb-2">
+                          <a href="/view/movies/$movie[short_url]" style="text-decoration:none; color: inherit">
+                              $movie[video_name]
+                          </a>
+                      </h2>
+                    
+                      <p class="L5 mb-0">
+                          <i class="mdi mdi-artist"></i> <span class="fs-16 mr-2 text-muted">$movie[category]</span>
+                      </p>
+                  </div>
+                </div>
+                <hr>
+              HTML;
             }
           }
         }
@@ -135,156 +189,21 @@ class Search{
                                     <a class="nav-link active" id="overview-tab" data-toggle="tab" href="#overview" role="tab" aria-controls="overview" aria-selected="true">Music</a>
                                   </li>
                                   <li class="nav-item">
-                                    <a class="nav-link" id="sales-tab" data-toggle="tab" href="#sales" role="tab" aria-controls="sales" aria-selected="false">Series</a>
+                                    <a class="nav-link" id="sales-tab" data-toggle="tab" href="#sales" role="tab" aria-controls="sales" aria-selected="false">Movies</a>
                                   </li>
                                   <li class="nav-item">
-                                    <a class="nav-link" id="purchases-tab" data-toggle="tab" href="#purchases" role="tab" aria-controls="purchases" aria-selected="false">Movies</a>
+                                    <a class="nav-link" id="purchases-tab" data-toggle="tab" href="#purchases" role="tab" aria-controls="purchases" aria-selected="false">TV Series</a>
                                   </li>
                                 </ul>
-                                <div class="tab-content py-0 px-0">
+                                <div class="tab-content py-3 px-5">
                                   <div class="tab-pane fade show active" id="overview" role="tabpanel" aria-labelledby="overview-tab">
-                                    <div class="d-flex flex-wrap justify-content-xl-between">
-                                      <div class="d-none d-xl-flex border-md-right flex-grow-1 align-items-center justify-content-center p-3 item">
-                                        <i class="mdi mdi-calendar-heart icon-lg mr-3 text-primary"></i>
-                                        <div class="d-flex flex-column justify-content-around">
-                                          <small class="mb-1 text-muted">Start date</small>
-                                          <div class="dropdown">
-                                            <a class="btn btn-secondary dropdown-toggle p-0 bg-transparent border-0 text-dark shadow-none font-weight-medium" href="#" role="button" id="dropdownMenuLinkA" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                              <h5 class="mb-0 d-inline-block">26 Jul 2018</h5>
-                                            </a>
-                                            <div class="dropdown-menu" aria-labelledby="dropdownMenuLinkA">
-                                              <a class="dropdown-item" href="#">12 Aug 2018</a>
-                                              <a class="dropdown-item" href="#">22 Sep 2018</a>
-                                              <a class="dropdown-item" href="#">21 Oct 2018</a>
-                                            </div>
-                                          </div>
-                                        </div>
-                                      </div>
-                                      <div class="d-flex border-md-right flex-grow-1 align-items-center justify-content-center p-3 item">
-                                        <i class="mdi mdi-currency-usd mr-3 icon-lg text-danger"></i>
-                                        <div class="d-flex flex-column justify-content-around">
-                                          <small class="mb-1 text-muted">Revenue</small>
-                                          <h5 class="mr-2 mb-0">$577545</h5>
-                                        </div>
-                                      </div>
-                                      <div class="d-flex border-md-right flex-grow-1 align-items-center justify-content-center p-3 item">
-                                        <i class="mdi mdi-eye mr-3 icon-lg text-success"></i>
-                                        <div class="d-flex flex-column justify-content-around">
-                                          <small class="mb-1 text-muted">Total views</small>
-                                          <h5 class="mr-2 mb-0">9833550</h5>
-                                        </div>
-                                      </div>
-                                      <div class="d-flex border-md-right flex-grow-1 align-items-center justify-content-center p-3 item">
-                                        <i class="mdi mdi-download mr-3 icon-lg text-warning"></i>
-                                        <div class="d-flex flex-column justify-content-around">
-                                          <small class="mb-1 text-muted">Downloads</small>
-                                          <h5 class="mr-2 mb-0">2233783</h5>
-                                        </div>
-                                      </div>
-                                      <div class="d-flex py-3 border-md-right flex-grow-1 align-items-center justify-content-center p-3 item">
-                                        <i class="mdi mdi-flag mr-3 icon-lg text-danger"></i>
-                                        <div class="d-flex flex-column justify-content-around">
-                                          <small class="mb-1 text-muted">Flagged</small>
-                                          <h5 class="mr-2 mb-0">3497843</h5>
-                                        </div>
-                                      </div>
-                                    </div>
+                                    $musicHtml
                                   </div>
                                   <div class="tab-pane fade" id="sales" role="tabpanel" aria-labelledby="sales-tab">
-                                    <div class="d-flex flex-wrap justify-content-xl-between">
-                                      <div class="d-none d-xl-flex border-md-right flex-grow-1 align-items-center justify-content-center p-3 item">
-                                        <i class="mdi mdi-calendar-heart icon-lg mr-3 text-primary"></i>
-                                        <div class="d-flex flex-column justify-content-around">
-                                          <small class="mb-1 text-muted">Start date</small>
-                                          <div class="dropdown">
-                                            <a class="btn btn-secondary dropdown-toggle p-0 bg-transparent border-0 text-dark shadow-none font-weight-medium" href="#" role="button" id="dropdownMenuLinkA" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                              <h5 class="mb-0 d-inline-block">26 Jul 2018</h5>
-                                            </a>
-                                            <div class="dropdown-menu" aria-labelledby="dropdownMenuLinkA">
-                                              <a class="dropdown-item" href="#">12 Aug 2018</a>
-                                              <a class="dropdown-item" href="#">22 Sep 2018</a>
-                                              <a class="dropdown-item" href="#">21 Oct 2018</a>
-                                            </div>
-                                          </div>
-                                        </div>
-                                      </div>
-                                      <div class="d-flex border-md-right flex-grow-1 align-items-center justify-content-center p-3 item">
-                                        <i class="mdi mdi-download mr-3 icon-lg text-warning"></i>
-                                        <div class="d-flex flex-column justify-content-around">
-                                          <small class="mb-1 text-muted">Downloads</small>
-                                          <h5 class="mr-2 mb-0">2233783</h5>
-                                        </div>
-                                      </div>
-                                      <div class="d-flex border-md-right flex-grow-1 align-items-center justify-content-center p-3 item">
-                                        <i class="mdi mdi-eye mr-3 icon-lg text-success"></i>
-                                        <div class="d-flex flex-column justify-content-around">
-                                          <small class="mb-1 text-muted">Total views</small>
-                                          <h5 class="mr-2 mb-0">9833550</h5>
-                                        </div>
-                                      </div>
-                                      <div class="d-flex border-md-right flex-grow-1 align-items-center justify-content-center p-3 item">
-                                        <i class="mdi mdi-currency-usd mr-3 icon-lg text-danger"></i>
-                                        <div class="d-flex flex-column justify-content-around">
-                                          <small class="mb-1 text-muted">Revenue</small>
-                                          <h5 class="mr-2 mb-0">$577545</h5>
-                                        </div>
-                                      </div>
-                                      <div class="d-flex py-3 border-md-right flex-grow-1 align-items-center justify-content-center p-3 item">
-                                        <i class="mdi mdi-flag mr-3 icon-lg text-danger"></i>
-                                        <div class="d-flex flex-column justify-content-around">
-                                          <small class="mb-1 text-muted">Flagged</small>
-                                          <h5 class="mr-2 mb-0">3497843</h5>
-                                        </div>
-                                      </div>
-                                    </div>
+                                    $moviesHtml
                                   </div>
                                   <div class="tab-pane fade" id="purchases" role="tabpanel" aria-labelledby="purchases-tab">
-                                    <div class="d-flex flex-wrap justify-content-xl-between">
-                                      <div class="d-none d-xl-flex border-md-right flex-grow-1 align-items-center justify-content-center p-3 item">
-                                        <i class="mdi mdi-calendar-heart icon-lg mr-3 text-primary"></i>
-                                        <div class="d-flex flex-column justify-content-around">
-                                          <small class="mb-1 text-muted">Start date</small>
-                                          <div class="dropdown">
-                                            <a class="btn btn-secondary dropdown-toggle p-0 bg-transparent border-0 text-dark shadow-none font-weight-medium" href="#" role="button" id="dropdownMenuLinkA" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                              <h5 class="mb-0 d-inline-block">26 Jul 2018</h5>
-                                            </a>
-                                            <div class="dropdown-menu" aria-labelledby="dropdownMenuLinkA">
-                                              <a class="dropdown-item" href="#">12 Aug 2018</a>
-                                              <a class="dropdown-item" href="#">22 Sep 2018</a>
-                                              <a class="dropdown-item" href="#">21 Oct 2018</a>
-                                            </div>
-                                          </div>
-                                        </div>
-                                      </div>
-                                      <div class="d-flex border-md-right flex-grow-1 align-items-center justify-content-center p-3 item">
-                                        <i class="mdi mdi-currency-usd mr-3 icon-lg text-danger"></i>
-                                        <div class="d-flex flex-column justify-content-around">
-                                          <small class="mb-1 text-muted">Revenue</small>
-                                          <h5 class="mr-2 mb-0">$577545</h5>
-                                        </div>
-                                      </div>
-                                      <div class="d-flex border-md-right flex-grow-1 align-items-center justify-content-center p-3 item">
-                                        <i class="mdi mdi-eye mr-3 icon-lg text-success"></i>
-                                        <div class="d-flex flex-column justify-content-around">
-                                          <small class="mb-1 text-muted">Total views</small>
-                                          <h5 class="mr-2 mb-0">9833550</h5>
-                                        </div>
-                                      </div>
-                                      <div class="d-flex border-md-right flex-grow-1 align-items-center justify-content-center p-3 item">
-                                        <i class="mdi mdi-download mr-3 icon-lg text-warning"></i>
-                                        <div class="d-flex flex-column justify-content-around">
-                                          <small class="mb-1 text-muted">Downloads</small>
-                                          <h5 class="mr-2 mb-0">2233783</h5>
-                                        </div>
-                                      </div>
-                                      <div class="d-flex py-3 border-md-right flex-grow-1 align-items-center justify-content-center p-3 item">
-                                        <i class="mdi mdi-flag mr-3 icon-lg text-danger"></i>
-                                        <div class="d-flex flex-column justify-content-around">
-                                          <small class="mb-1 text-muted">Flagged</small>
-                                          <h5 class="mr-2 mb-0">3497843</h5>
-                                        </div>
-                                      </div>
-                                    </div>
+                                    
                                   </div>
                                 </div>
                               </div>

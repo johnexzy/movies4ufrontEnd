7e287f5ebf4f1a9449da224fd23b2c1f5c7b9845
Johnexzy <obajohn75@gmail.com>
2020-10-20 17:57:35 +0100

    :lipstick: (changed  views)

--- index.php
+++ index.php
@@ -119,7 +119,7 @@ foreach ($movies as $i=>$movie) {
       <div class="content-wrapper">
         
         <div class="container">
-          <!-- <div class="d-flex justify-content-center text-light text-center m-3" data-aos="fade-down"><!--
+          <!-- <div class="d-flex justify-content-center text-light text-center m-3" data-aos="fade-down">
             <h3 class="text-uppercase font-weight-600 shadow">
               Get the Latest Movies, Music and Series for Free on Leccel.net
             </h3>
@@ -139,22 +139,23 @@ foreach ($movies as $i=>$movie) {
                           $short_url = $music["short_url"];
                           $image = $music["images"][0];
                           echo<<<HTML
-                              <div
-                                  class="d-flex justify-content-start border-bottom mt-2" 
-                                  style="cursor:pointer"
-                                  >
-                                  <input type="hidden" value="/view/music/$short_url">
-                                  <a 
-                                    class="h3 font-weight-200 mb-1" 
-                                    href="/view/music/$short_url"
-                                    style="text-decoration:none; color: inherit"
-                                    ><i class="mdi mdi-star-circle text-danger"></i>
-                                      <h4 class=" d-inline font-weight-200 mb-0">
-                                        <img src="http://127.0.0.1:8090/$image" style="width:60px; height:60px" alt="" class="card-img d-inline">
-                                        <p class="d-inline ml-1">$music[music_name]</p>
-                                      </h4>
-                                  </a>
-                              </div>
+                              <a 
+                              class="h3 font-weight-200 mb-1" 
+                              href="/view/music/$short_url"
+                              style="text-decoration:none; color: inherit"
+                              >
+                                <div
+                                class="d-flex justify-content-start border-bottom mt-2 mb-2 shadow" 
+                                style="cursor:pointer"
+                                >
+                                  <i class="mdi mdi-star-circle text-danger"></i>
+                                  <h4 class=" d-inline font-weight-200 mb-0">
+                                    <img src="http://127.0.0.1:8090/$image" style="width:60px; height:60px" alt="" class="card-img d-inline">
+                                    
+                                  </h4>
+                                  <p class="d-inline ml-1 font-weight-bold text-primary">$music[music_name]</p>
+                                </div>
+                            </a>
                           HTML;
                         }
                       ?>
@@ -184,66 +185,67 @@ foreach ($movies as $i=>$movie) {
                 </div>
                 <div class="card-body">
                   
-                        <?php
-                            foreach ($audios as $music) {
-                              $image = $music["images"][0];
-                              $short_url = $music["short_url"];
-                              $commentCount = count($music["comments"]);
-                                echo<<<HTML
-                                    <a 
-                                        class="h3 font-weight-200 mb-1" 
-                                        href="/view/music/$short_url"
-                                        style="text-decoration:none; color: inherit"
-                                        >
-                                        <div
-                                        class="d-flex justify-content-start border-bottom mt-2 mb-2 shadow" 
-                                        style="cursor:pointer"
-                                        >
-                                         <h4 class=" d-inline font-weight-200 mb-0">
-                                              <img src="http://127.0.0.1:8090/$image" style="width:60px; height:60px" alt="" class="card-img d-inline">
-                                              <p class="d-inline ml-1 font-weight-bold text-primary">Download (MP3) - $music[music_name]</p>
-                                            </h4>
-                                        
-                                    </div></a>
-                                    <!-- <div class="col-md-4 grid-margin stretch-card">
-                                      <div class="card card-rounded shadow music">
-                                        <a href="/view/music/$short_url"
-                                            class="text-decoration-none">
-                                            <div class="card-img-holder">
-                                              <img src="http://127.0.0.1:8090/$image" alt="" class="card-img">
-                                            </div>
-                                        </a>
-                                        
-                                        <div class="card-body p-2" style="background:#eee;">
+                <?php
+                    foreach ($audios as $music) {
+                      $image = $music["images"][0];
+                      $short_url = $music["short_url"];
+                      $commentCount = count($music["comments"]);
+                        echo<<<HTML
+                            <a 
+                              class="h3 font-weight-200 mb-1" 
+                              href="/view/music/$short_url"
+                              style="text-decoration:none; color: inherit"
+                              >
+                                <div
+                                class="d-flex justify-content-start border-bottom mt-2 mb-2 shadow" 
+                                style="cursor:pointer"
+                                >
+                                  <h4 class=" d-inline font-weight-200 mb-0">
+                                    <img src="http://127.0.0.1:8090/$image" style="width:60px; height:60px" alt="" class="card-img d-inline">
+                                    
+                                  </h4>
+                                  <h4 class="d-inline ml-1 font-weight-bold text-primary">Download (MP3) - $music[music_name]</h4>
+                                </div>
+                            </a>
+                            <!-- <div class="col-md-4 grid-margin stretch-card">
+                              <div class="card card-rounded shadow music">
+                                <a href="/view/music/$short_url"
+                                    class="text-decoration-none">
+                                    <div class="card-img-holder">
+                                      <img src="http://127.0.0.1:8090/$image" alt="" class="card-img">
+                                    </div>
+                                </a>
+                                
+                                <div class="card-body p-2" style="background:#eee;">
+                                <a 
+                                  class="h3 mb-0" 
+                                  href="/view/music/$short_url"
+                                  style="text-decoration:none; color: inherit"
+                                  >
+                                    <h3 class="font-weight-200 mb-2" style="color:#561529">
+                                      (Download MP3) - $music[music_name]
+                                    </h3>
+                                    </a>
+                                    <div class="d-flex justify-content-between">
+                                      <p class="d-inline L5 mb-0">
+                                        <i class="mdi mdi-artist"></i>
                                         <a 
-                                          class="h3 mb-0" 
-                                          href="/view/music/$short_url"
-                                          style="text-decoration:none; color: inherit"
-                                          >
-                                            <h3 class="font-weight-200 mb-2" style="color:#561529">
-                                              (Download MP3) - $music[music_name]
-                                            </h3>
-                                            </a>
-                                            <div class="d-flex justify-content-between">
-                                              <p class="d-inline L5 mb-0">
-                                                <i class="mdi mdi-artist"></i>
-                                                <a 
-                                                  href="/view/search/$music[artist]"
-                                                  target="_blank" 
-                                                  class="fs-15 text-muted text-decoration-none">
-                                                  $music[artist]
-                                                  </a>
-                                              </p>
-                                              <p class="d-inline mb-0">
-                                                <i class="mdi mdi-comment"></i>($commentCount)
-                                              </p>
-                                            </div>
-                                        </div>
-                                      </div>
-                                    </div> -->
-                                HTML;
-                            }
-                          ?>
+                                          href="/view/search/$music[artist]"
+                                          target="_blank" 
+                                          class="fs-15 text-muted text-decoration-none">
+                                          $music[artist]
+                                          </a>
+                                      </p>
+                                      <p class="d-inline mb-0">
+                                        <i class="mdi mdi-comment"></i>($commentCount)
+                                      </p>
+                                    </div>
+                                </div>
+                              </div>
+                            </div> -->
+                        HTML;
+                    }
+                  ?>
                       
                       <div class="d-flex justify-content-center align-content-center mt-5 mb-0">
                         <a href="/pages/music.html" class="btn btn-info shadow">ALL MUSIC</a>
